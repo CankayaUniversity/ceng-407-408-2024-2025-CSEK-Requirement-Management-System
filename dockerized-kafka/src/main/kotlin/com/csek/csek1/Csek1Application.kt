5f295a26f751be9912871bb4e072b3289a818d74
Eray Emir <128998801+peartolsamia@users.noreply.github.com>
2025-04-23 11:49:22 +0300

    kafka dockerized files

---
+++ dockerized-kafka/src/main/kotlin/com/csek/csek1/Csek1Application.kt
@@ -0,0 +1,11 @@
+package com.csek.csek1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Csek1Application
+
+fun main(args: Array<String>) {
+	runApplication<Csek1Application>(*args)
+}
